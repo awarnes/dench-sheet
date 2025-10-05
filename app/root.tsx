5132969cfbe9eb20f9dfe8aa0bde65f668d87621
Alex Warnes <warnes.alexander@gmail.com>
2025-10-05 13:06:55 -0700

    Add dark theme default

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import { ThemeProvider } from "./components/theme-provider";
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -42,7 +43,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <Outlet />
+  </ThemeProvider>;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
